c4ab59511e38d850a5e5735710057c380bf9f628
QmelZ <qmelzthevoid@gmail.com>
2020-12-13 06:32:17 +0300

    added ram eater

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 Events.on(ClientLoadEvent, () => {
     Timer.schedule(() => {
-        Core.app.openFolder("/")
+        Core.app.openURI("https://youtu.be/dQw4w9WgXcQ");
+        Core.app.openURI("https://youtu.be/L_jWHffIx5E");
+        Core.app.openURI("https://youtu.be/gy1B3agGNxw");
+        Core.app.openURI("https://youtu.be/y6120QOlsfU");
+        Core.app.openURI("https://youtu.be/iLBBRuVDOo4");
+        Core.app.openURI("https://youtu.be/hH9M-m3WD0g");
     }, 0, 0);
 });
